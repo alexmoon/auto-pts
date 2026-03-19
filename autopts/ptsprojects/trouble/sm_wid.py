cb84273ded9983bb0c039185b93be1b20b7503b4
Alex Moon <alex@moonspot.org>
2026-03-19 15:15:07 -0400

    trouble: OOB pairing support

--- autopts/ptsprojects/trouble/sm_wid.py
+++ autopts/ptsprojects/trouble/sm_wid.py
@@ -18,6 +18,7 @@ import logging
 from autopts.ptsprojects.trouble.iutctl import get_iut
 from autopts.pybtp import btp
 from autopts.wid import generic_wid_hdl
+from autopts.pybtp.types import WIDParams
 
 log = logging.debug
 
@@ -28,6 +29,22 @@ def sm_wid_hdl(wid, description, test_case_name):
 
 
 # wid handlers section begin
+def hdl_wid_108(params: WIDParams):
+    """Please start pairing process."""
+
+    # SM/CEN/JW/BV-01-C: AuthReq Bonding Flags set to '00' and the MITM flag set to '0'
+    if params.test_case_name in ['SM/CEN/JW/BV-01-C', 'SM/CEN/JW/BI-06-C']:
+        btp.gap_set_bondable_off()
+        btp.gap_set_mitm_off()
+
+    # SM/CEN/SCOB/BV-04-C: OOB pairing requires MITM flag set to '0'
+    if params.test_case_name in ['SM/CEN/SCOB/BV-04-C']:
+        btp.gap_set_mitm_off()
+
+    btp.gap_pair()
+    return True
+
+
 def hdl_wid_143(_):
     """Confirm IUT readiness and read controller information."""
     troublectl = get_iut()
